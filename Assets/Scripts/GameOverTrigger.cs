bb2e041fc450933eac9ee7042d7e7c46370d4dfc
prasanthgajula8 <prasanth080898@gmail.com>
2018-11-13 15:00:39 +0530

    Added GameOver functionality

---
+++ Assets/Scripts/GameOverTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverTrigger : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject[] Gates;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="BoyGates"||collision.gameObject.tag=="GirlGates")
+        {
+            panel.SetActive(true);
+            Time.timeScale = 0;
+            foreach(GameObject i in Gates)
+            {
+                i.GetComponent<GateMovement>().movementSpeed = 0;
+            }
+        }
+    }
+}
--- Assets/Scripts/GateMovement.cs
+++ Assets/Scripts/GateMovement.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GateMovement : MonoBehaviour
 {
-    //public bool rightGate;
-    //public bool leftGate;
     public float movementSpeed;
     // Use this for initialization
     void Start()
@@ -16,14 +14,6 @@ public class GateMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(leftGate)
-        //{
-            this.transform.Translate(Vector2.left * movementSpeed);
-        //}
-
-        //else
-        //{
-        //    this.transform.Translate(Vector2.left * movementSpeed);
-        //}
+        this.transform.Translate(Vector2.left * movementSpeed);
     }
 }
