e413e0e2239cb9d261d497a94a7a568a0fd6ffbd
prasanthgajula8 <prasanth080898@gmail.com>
2018-11-13 14:34:38 +0530

    Added GateMechanics and UI Assets

---
+++ Assets/Scripts/GateMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateMovement : MonoBehaviour
+{
+    //public bool rightGate;
+    //public bool leftGate;
+    public float movementSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(leftGate)
+        //{
+            this.transform.Translate(Vector2.left * movementSpeed);
+        //}
+
+        //else
+        //{
+        //    this.transform.Translate(Vector2.left * movementSpeed);
+        //}
+    }
+}
