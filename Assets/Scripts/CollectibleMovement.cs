1358f8d093656fc9861dae78214aab6f4613dac1
prasanthgajula8 <prasanth080898@gmail.com>
2018-11-13 14:16:55 +0530

    Added prefabs and scripts folder with spawning mechanic ingame

---
+++ Assets/Scripts/CollectibleMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector2.up * movementSpeed);
+    }
+}
---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject collectible;
+    public float repeatRate;
+    public float startTime;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("InstantiateCollectible", startTime, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InstantiateCollectible()
+    {
+        Instantiate(collectible,this.gameObject.transform);
+    }
+}
